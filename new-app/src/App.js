78d298faac697e2c7522888cc44af07084aebe40
Simon Brooks <simon.gmbrooks@gmail.com>
2019-02-23 17:51:28 +0000

    Integrated skeleton frontend

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Location from './components/Location.js'
+import Emergencies from './components/Emergencies.js'
 import {geolocated} from 'react-geolocated';
 
 class App extends Component {
+  state = {
+    emergencies: [
+      {
+        id: 1,
+        name: 'Simon Brooks',
+        distance: '450m',
+        expanded: true
+      },
+      {
+        id: 2,
+        name: 'Chris Coles',
+        distance: '666m',
+        expanded: false
+      },
+      {
+        id: 3,
+        name: 'Vlad Secosan',
+        distance: '72m',
+        expanded: false
+      }
+    ]
+  }
 
   getLocation = () =>{
     return (
@@ -16,6 +39,8 @@ class App extends Component {
     return (
       <div className="App">
       
+      <Emergencies emergencies={this.state.emergencies}/>
+
       <Location/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
