ad64a9df1050f6c36e97fdc33770c114f9672d1a
Christopher Coles <cbc1g16@soton.ac.uk>
2019-02-24 09:02:06 +0000

    docs build

---
+++ docs/precache-manifest.3ed82034ddf6de586effa71f24839edd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc9522073bce890aa1c489fe70fc9674",
+    "url": "/static/media/Logo_Font_Slogan.dc952207.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "02a7ca0ae618ffbb8f1d",
+    "url": "/static/js/main.02a7ca0a.chunk.js"
+  },
+  {
+    "revision": "1aa7c8deecbb6606d1dc",
+    "url": "/static/js/2.1aa7c8de.chunk.js"
+  },
+  {
+    "revision": "02a7ca0ae618ffbb8f1d",
+    "url": "/static/css/main.7facb5a7.chunk.css"
+  },
+  {
+    "revision": "ecc046e61b30bf7d20ae153e4a962ea0",
+    "url": "/index.html"
+  }
+];
