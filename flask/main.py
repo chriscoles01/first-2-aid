506e6a4c7833eefc052da1cb3d513f5ff4e13025
Christopher Coles <cbc1g16@soton.ac.uk>
2019-02-24 10:34:25 +0000

    [fix] max tweets increased

--- flask/main.py
+++ flask/main.py
@@ -41,7 +41,7 @@ CORS(app)
 class GetList(Resource):
     def get(self):
         api = tweepy.API(auth)
-        max_tweets=5
+        max_tweets=20
         query='#first2me'
         searched_tweets = [status._json for status in tweepy.Cursor(api.search,  q=query).items(max_tweets)]
         json_strings = [json.dumps(json_obj) for json_obj in searched_tweets]  
