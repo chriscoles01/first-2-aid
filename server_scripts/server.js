2643606d7ec5443d62136e5d888cc412c24ccc6c
Christopher Coles <cbc1g16@soton.ac.uk>
2019-02-24 11:53:53 +0000

    [Feat] post request for reply

--- server_scripts/server.js
+++ server_scripts/server.js
@@ -5,6 +5,7 @@ const session = require('express-session')
 const cors = require('cors')
 const socketio = require('socket.io')
 const { Strategy: TwitterStrategy } = require('passport-twitter')
+var Twit = require('twit')
 
 // Private api keys that you will get when registering an app on 
 // apps.twitter.com
@@ -15,8 +16,15 @@ const TWITTER_CONFIG = {
   // when registering the app
   callbackURL: 'http://127.0.0.1:8080/twitter/callback'
 }
-
-// Create the server and allow express and sockets to run on the same port
+var T = new Twit({
+  consumer_key:         'Q4sZ0b7kjLhVW7zLo8dGNSsFw',
+  consumer_secret:      'bzgCJ2a2zMQYZ9OzB3ueLuPDvliUSu4S5fMBGbEBwzkexoB10n',
+  access_token:         '2875036372-rEBuUQbpuV0edFfAMxYoI8J6hwXrraOT4SSyIjR',
+  access_token_secret:  'bEV75aRZaKbYqmJYxs7svx4cSWRg4r4gzj2fzpa5n8W40',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+})
+// Create the server and allow express and sockets to run on the same hyport
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
@@ -67,19 +75,37 @@ const addSocketIdToSession = (req, res, next) => {
   next()
 }
 
+
 // This is endpoint triggered by the popup on the client which starts the whole
 // authentication process
 app.get('/twitter', addSocketIdToSession, twitterAuth)
 
+app.post('/twitter/reply', (req, res) => {
+  
+  
+
+  string_status = 'first responder coming  @' + req.body.username
+  T.post('statuses/update', {in_reply_to_status_id: req.body.id, status: string_status }, function(err, data, response) {
+    console.log(data)
+  })
+ return res.status(201).send({
+   success: 'true',
+   message: 'reply done',
+   
+ })
+});
 // This is the endpoint that Twitter sends the user information to. 
 // The twitterAuth middleware attaches the user to req.user and then
 // the user info is sent to the client via the socket id that is in the 
 // session. 
 app.get('/twitter/callback', twitterAuth, (req, res) => {
+  
   io.in(req.session.socketId).emit('user', req.user)
   res.end()
 })
 
+
 server.listen(8080, () => {
   console.log('listening...')
 })
+
