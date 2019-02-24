5bf98b9d805360ed8f524291d6c4522bd8e472c7
Sergiu Lazarescu <sl1g18@soton.ac.uk>
2019-02-24 10:32:21 +0000

    [Added] Better twitter pop-up window

--- new-app/src/components/Login.js
+++ new-app/src/components/Login.js
@@ -33,10 +33,18 @@ export default class App extends Component {
   }
 
   openPopup() {
-    const url = `${API_URL}/twitter?socketId=${socket.id}`
-
-    return window.open(url, '')
-  }
+      const width = 600, height = 600
+      const left = (window.innerWidth / 2) - (width / 2)
+      const top = (window.innerHeight / 2) - (height / 2)
+      
+      const url = `${API_URL}/twitter?socketId=${socket.id}`
+  
+      return window.open(url, '',       
+        `toolbar=no, location=no, directories=no, status=no, menubar=no, 
+        scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
+        height=${height}, top=${top}, left=${left}`
+      )
+    }
 
   startAuth() {
     if (!this.state.disabled) {  
